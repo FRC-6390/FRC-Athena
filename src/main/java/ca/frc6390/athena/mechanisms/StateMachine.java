6a4fccf2167ebfc59b2907103ee6239d33420f54
Mathias <70431366+SoZ0@users.noreply.github.com>
2025-02-09 19:55:55 -0800

    state machine fixes

--- src/main/java/ca/frc6390/athena/mechanisms/StateMachine.java
+++ src/main/java/ca/frc6390/athena/mechanisms/StateMachine.java
@@ -7,18 +7,14 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 public class StateMachine<E extends Enum<E>> {
     
-    private Trigger stateChanger;
     private E goalState, nextState;
     private BooleanSupplier atStateSupplier, changeStateSupplier;
 
     public StateMachine(E initialState, BooleanSupplier atStateSupplier){
+        this.goalState = initialState;
+        this.nextState = initialState;
         this.atStateSupplier = atStateSupplier;
         this.changeStateSupplier = () -> true;
-        this.stateChanger = new Trigger(this::isAtState);
-        this.stateChanger.onTrue(new InstantCommand(() -> 
-        {
-            goalState = nextState;
-        }));
         setGoalState(initialState);
     }
 
@@ -31,12 +27,20 @@ public class StateMachine<E extends Enum<E>> {
         changeStateSupplier = condition;
     }
 
-    private boolean isAtState() {
-        return this.changeStateSupplier.getAsBoolean();
+    public BooleanSupplier getChangeStateSupplier(){
+        return changeStateSupplier;
+    }
+
+    private boolean shouldChangeState() {
+        return changeStateSupplier.getAsBoolean();
     }
 
     public E getGoalState() {
         return goalState;
+    }
+
+    public E getNextState() {
+        return nextState;
     }
 
     public boolean atGoalState() {
@@ -45,5 +49,11 @@ public class StateMachine<E extends Enum<E>> {
 
     public boolean atState(E state) {
         return atGoalState() && state == goalState;
+    }
+
+    public void update() {
+        if(shouldChangeState()){
+            goalState = nextState;
+        }
     }
 }
