8947699ff98cdd6cc4934c104f2807d316851347
HephaestusRobotics <robotics.hephaestus@gmail.com>
2026-03-03 21:15:32 -0800

    puash

--- athena-core/src/main/java/ca/frc6390/athena/commands/control/SwerveDriveCommand.java
+++ athena-core/src/main/java/ca/frc6390/athena/commands/control/SwerveDriveCommand.java
@@ -8,6 +8,7 @@ import ca.frc6390.athena.core.RobotSpeeds;
 import ca.frc6390.athena.drivetrains.swerve.SwerveDrivetrain;
 import ca.frc6390.athena.hardware.motor.MotorNeutralMode;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 
 public class SwerveDriveCommand extends Command {
@@ -17,6 +18,7 @@ public class SwerveDriveCommand extends Command {
   private final BooleanSupplier fieldRelativeSupplier;
   //Double suppliers are outputted by the joystick
   private final DoubleSupplier xInput, yInput, thetaInput;
+  private double lastTime;
 
   public SwerveDriveCommand(SwerveDrivetrain driveTrain, DoubleSupplier xInput, DoubleSupplier yInput, DoubleSupplier thetaInput, boolean fieldRelative) {
     this(driveTrain, xInput, yInput, thetaInput, () -> fieldRelative);
@@ -35,10 +37,15 @@ public class SwerveDriveCommand extends Command {
     this.fieldRelativeSupplier = Objects.requireNonNull(fieldRelativeSupplier, "fieldRelativeSupplier");
     addRequirements(driveTrain);
     driveTrain.hardware().neutralMode(MotorNeutralMode.Brake);
+    SmartDashboard.putNumber("FudgeFactor", 1.3);
+    lastTime = edu.wpi.first.wpilibj.Timer.getFPGATimestamp();
   }
   
   @Override
-  public void initialize() {
+  public void initialize() 
+  {
+
+
   }
 
   @Override
@@ -48,9 +55,26 @@ public class SwerveDriveCommand extends Command {
     double ySpeed = yInput.getAsDouble() * driveTrain.speeds().maxVelocity();
     double thetaSpeed = thetaInput.getAsDouble() * driveTrain.speeds().maxAngularVelocity();
 
+    double now = edu.wpi.first.wpilibj.Timer.getFPGATimestamp();
+    double dt = now - lastTime;
+    lastTime = now;
+
+    double fudge = SmartDashboard.getNumber("FudgeFactor", 1.3);
+    double alpha = fudge * thetaSpeed * dt / 2.0;
+
+    SmartDashboard.putNumber("Loop Time", dt);
+
+
+    double cosA = Math.cos(-alpha);
+    double sinA = Math.sin(-alpha);
+
+    double x = xSpeed * cosA - ySpeed * sinA;
+    double y = xSpeed * sinA + ySpeed * cosA;
+
+
     ChassisSpeeds chassisSpeeds = fieldRelativeSupplier.getAsBoolean()
-        ? ChassisSpeeds.fromFieldRelativeSpeeds(xSpeed, ySpeed, thetaSpeed, driveTrain.imu().device().getVirtualAxis("driver"))
-        : new ChassisSpeeds(xSpeed, ySpeed, thetaSpeed);
+        ? ChassisSpeeds.fromFieldRelativeSpeeds(x, y, thetaSpeed, driveTrain.imu().device().getVirtualAxis("driver"))
+        : new ChassisSpeeds(x, y, thetaSpeed);
 
     driveTrain.speeds().set(RobotSpeeds.DRIVE_SOURCE, chassisSpeeds);
   }
